5c41dc0f59291326ee8e123f4b294af257417b6f
Suk <59326248+Suk-ldev@users.noreply.github.com>
2024-11-10 12:38:48 +0800

    更新 QrCard.tsx中文

--- components/QrCard.tsx
+++ components/QrCard.tsx
@@ -24,7 +24,7 @@ export const QrCard: React.FC<QrCardProps> = ({ imageURL, time }) => {
         height={480}
       />
       <p className="text-gray-400 text-sm italic">
-        QR code took {time} seconds to generate.
+        花费了 {time} 秒来生成二维码.
       </p>
     </div>
   );
