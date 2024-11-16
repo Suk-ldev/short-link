cb8bf6a82c99e16a4eaad704eecfdc1510384f7c
Suk <59326248+Suk-ldev@users.noreply.github.com>
2024-11-16 18:43:01 +0800

    更新 route.ts

--- app/api/s/[code]/route.ts
+++ app/api/s/[code]/route.ts
@@ -8,12 +8,12 @@ export async function GET(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
     const code = pathname.split('/').pop();
     if (!code) {
-        return NextResponse.json({ message: 'URL Invalid' });
+        return NextResponse.json({ message: '网址无效' });
     }
 
     const data = (await kv.get(code)) as ShortLinkData
     if (!data || !data.url) {
-        return NextResponse.json({ message: 'URL not found' }, { status: 404 });
+        return NextResponse.json({ message: '找不到网址' }, { status: 404 });
     }
 
     return NextResponse.redirect(data.url, 302);
